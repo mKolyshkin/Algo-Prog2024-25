672244ee3a4781b5e0fdd85530e6121eea59ea74
mKolyshkin <m.y.kolyshkin@student.khai.edu>
2024-09-11 13:00:50 +0300

    Lab1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// EX Begin8
+	
+	unsigned int a, b, Am; // variables
+	
+	cout << "Enter a: "; // introduct var 'a'
+	cin >> a;
+	cout << "Enter b: "; // introduct var 'b'
+	cin >> b;
+	
+	Am = (a+b)/2; // result calculation
+	
+	cout << "Arithmetic mean: " << Am << endl << endl; // result output
+	
+	// EX Begin28
 
-#include <iostream>  // Підключення ст бібліотеки
-using namespace std; // Простір імен std
+	unsigned int V1, V2, S, T;  // variables
 
-int main() {
+	cout << "Enter V1 (km/h): "; // introduct var 'V1' - speed car1 
+	cin >> V1;
+	cout << "Enter V2 (km/h): "; // introduct var 'V2' - speed car2 
+	cin >> V2;
+	cout << "Enter T (hours): "; // introduct var 'T' - time
+	cin >> T;
 
-    int a; // Змінна "а"
+	S= V1*T + V2*T; // result calculation total distance
 
-    cout << "Enter value a: "; // Запит на введення "a"
-    cin >> a; // Введення значення 'a' з консолі
+	cout << "Total distance between cars: " << S << endl; // result output
 
-    int res = a - 5; // Обчислення результату: "a-5"
-    cout << "a - 5: " << res << endl; // Виведення результату до консолі і перехід на новий рядок
-
-        return 0; // Повернення успішного завершення програми
+	return 0;
 }
