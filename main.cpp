26a5006d9eead85c1757333a82e841cb29f10ba7
mKolyshkin <m.y.kolyshkin@student.khai.edu>
2024-09-11 13:08:37 +0300

    ver2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	// EX Begin8
 	
-	unsigned int a, b, Am; // variables
-	
+	double a, b, Am; // variables
+
 	cout << "Enter a: "; // introduct var 'a'
 	cin >> a;
 	cout << "Enter b: "; // introduct var 'b'
@@ -26,7 +26,7 @@ int main()
 	cout << "Enter T (hours): "; // introduct var 'T' - time
 	cin >> T;
 
-	S= V1*T + V2*T; // result calculation total distance
+	S= V1*T + V2*T; // total distance calculation
 
 	cout << "Total distance between cars: " << S << endl; // result output
 
